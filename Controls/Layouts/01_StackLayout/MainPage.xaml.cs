cad13a76aad1c22c1c0cafa670b8bb6aa819b0a5
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-02 08:27:57 +0530

    MAUI Controls Layout --> Stack Layout is updated

---
+++ Controls/Layouts/01_StackLayout/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_StackLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ Controls/Layouts/01_StackLayout/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _01_StackLayout
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

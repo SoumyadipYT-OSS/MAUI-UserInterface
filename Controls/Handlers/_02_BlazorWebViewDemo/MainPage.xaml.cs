1daccb3da5342b7b1b50b35303ec909922d561fd
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-03 23:00:12 +0530

    updated

---
+++ Controls/Handlers/_02_BlazorWebViewDemo/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_BlazorWebViewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

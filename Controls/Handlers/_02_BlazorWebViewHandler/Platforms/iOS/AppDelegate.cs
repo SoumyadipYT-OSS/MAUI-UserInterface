6cb4a74f40e141300e912f5bbda95305fddfd11b
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-03 21:11:16 +0530

    Microsoft.AspNetCore.Components.WebView.Maui package is installed

---
+++ Controls/Handlers/_02_BlazorWebViewHandler/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _02_BlazorWebViewHandler
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

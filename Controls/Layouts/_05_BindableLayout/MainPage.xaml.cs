c0a0bc956b9d2d4bc4cfdc57345287a3759a1ad2
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-03 07:21:39 +0530

    BindableLayout is updated

---
+++ Controls/Layouts/_05_BindableLayout/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using _05_BindableLayout.ViewModel;
+
+
+namespace _05_BindableLayout 
+{
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ Controls/Layouts/_05_BindableLayout/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace _05_BindableLayout.ViewModel {
+    public class MainPageViewModel : BindableObject {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public MainPageViewModel() {
+            Items = new ObservableCollection<Item> { 
+                new() {Name = "Item 1"},
+                new() {Name = "Item 2"},
+                new() {Name = "Item 3"}
+            };
+        }
+    }
+
+    public class Item {
+        public string? Name { get; set; }
+    }
+}

b5ccc7d7e1de63f4bfbc0a414337672f5dc9ae1e
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-13 22:35:45 +0530

    _05_Button Class of Handlers is complete

---
+++ Controls/Handlers/_05_Button/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace _05_Button {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+            AnimateButton();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e) {
+            // Handle the button click event
+            DisplayAlert("Button Clicked", "You clicked the button!", "OK");
+        }
+        private async void OnButtonFontAtrribute(object sender, EventArgs e) {
+            colorChangingButton.BackgroundColor = Colors.Green;
+            await DisplayAlert("Button Clicked!", "You clicked the button!", "OK");
+        }
+
+        private async void AnimateButton() {
+            while (true) {
+                await animatedButton.ScaleTo(1.2, 500);
+                await animatedButton.ScaleTo(1.0, 500);
+            }
+        }
+    }
+}

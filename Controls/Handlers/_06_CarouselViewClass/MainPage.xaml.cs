f0da355d632ca59e0f7bd1d6be96a62d199dd9bd
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-14 01:33:04 +0530

    _06_CarouselView is completed

---
+++ Controls/Handlers/_06_CarouselViewClass/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.Timers;
+
+namespace _06_CarouselViewClass {
+    public partial class MainPage : ContentPage {
+        public ObservableCollection<string> Images { get; set; }
+        
+        // making readonly can improve code safety and clarity
+        private readonly System.Timers.Timer _timer;
+
+        public MainPage() {
+            InitializeComponent();
+            Images = [
+                "Resources/Images/avocado.svg",
+                "Resources/Images/grape.svg",
+                "Resources/Images/kiwi.svg",
+                "Resources/Images/lemon.svg",
+                "Resources/Images/waterlemon.svg"
+            ];
+            BindingContext = this;
+
+            // Initialize and start the timer for automatic scrolling
+            _timer = new System.Timers.Timer(2000); // Change slide every 2 seconds
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.Start();
+        }
+
+
+        private void OnTimerElapsed(object? sender, ElapsedEventArgs? e) {
+            Dispatcher.Dispatch(() => {
+                // Update the CarouselView if not dragging
+                if (!modernCarouselView.IsDragging) {
+                    UpdateCarouselView(modernCarouselView);
+                }
+            });
+        }
+
+        private void UpdateCarouselView(CarouselView carouselView) {
+            if (carouselView.Position == Images.Count - 1) {
+                carouselView.Position = 0;
+            } else {
+                carouselView.Position += 1;
+            }
+        }
+
+        private void OnPositionChanged(object sender, PositionChangedEventArgs e) {
+            // Update the IndicatorView when the position changes
+            modernIndicatorView.SelectedIndicatorColor = Colors.Black;
+        }
+
+        protected override void OnDisappearing() {
+            base.OnDisappearing();
+            _timer.Stop();
+            _timer.Dispose();
+        }
+    }
+}
---
+++ Controls/Handlers/_06_CarouselViewClass/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _06_CarouselViewClass
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

74b46a42dac0fad03457b2e6e7484a37bc51b3aa
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-12-14 03:48:41 +0530

    _07_CellClass is uploaded complete

---
+++ Controls/Handlers/_07_CellClass/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace _07_CellClass 
+{
+    public partial class MainPage : ContentPage {
+        public bool NotificationsEnabled { get; set; }
+        public double SliderValue { get; set; }
+
+        public MainPage() {
+            InitializeComponent();
+            NotificationsEnabled = true;
+            SliderValue = 50;
+            BindingContext = this;
+        }
+    }
+}
---
+++ Controls/Handlers/_07_CellClass/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _07_CellClass
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

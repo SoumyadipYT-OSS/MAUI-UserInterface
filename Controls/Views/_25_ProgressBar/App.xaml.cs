ef75a9569c289cd13d68733c8759b1470b8f98df
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-03-17 20:57:55 +0530

    _25 ProgressBar is updated

---
+++ Controls/Views/_25_ProgressBar/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace _25_ProgressBar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
---
+++ Controls/Views/_25_ProgressBar/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace _25_ProgressBar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnUpdateProgressClicked(object? sender, EventArgs e) {
+            if (double.TryParse(progressEntry.Text, out double inputProgress) && inputProgress >= 0  &&  inputProgress <= 100) {
+                double progress = inputProgress / 100;      // Convert the value between 0 and 1
+                await Ex3PB.ProgressTo(/*local var*/ progress, /* time: 32 bit int*/ 500, /*built-in ease function*/ Easing.Linear);
+            } else {
+                await DisplayAlert("Invalid Input", "Please enter a valid number between 0 and 100.", "OK");
+            }
+        }
+    }
+}

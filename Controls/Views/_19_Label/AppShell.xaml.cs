4d4524a486e60db83df313d86c4fcd3a1a539f91
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-03-17 20:42:55 +0530

    _19 Label Class is updated

---
+++ Controls/Views/_19_Label/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _19_Label
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Controls/Views/_19_Label/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _19_Label
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}

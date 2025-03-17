0761f74e4ed47566f81223360bd34c4fa7a699ef
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-03-17 20:19:22 +0530

    _13 Entry Class is added

---
+++ Controls/Views/_13_Entry/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace _13_Entry
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Setting teh Entry under Constructor of the class
+            Microsoft.Maui.Controls.Entry entry = new Microsoft.Maui.Controls.Entry { Placeholder = "Enter text" };
+            entry.TextChanged += OnEntryTextChanged;
+            entry.Completed += OnEntryCompleted;
+        }
+
+
+        private void OnEntryTextChanged(object? sender, TextChangedEventArgs e) {
+            _ = e.OldTextValue;
+            _ = e.NewTextValue;
+            _ = entry.Text;
+        }
+
+        private void OnEntryCompleted(object? sender, EventArgs e) {
+            if (sender is Entry entry  &&  entry.Text != null) {
+                string text = entry.Text;
+
+                DisplayAlert("Completed", $"You entered: {text}", "OK");
+            }
+        }
+    }
+
+}
---
+++ Controls/Views/_13_Entry/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _13_Entry
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

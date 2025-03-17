a2061a53e154ad2c3282a02414662917728a0d93
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-03-17 20:57:09 +0530

    _24 Picker Class is updated

---
+++ Controls/Views/_24_Picker/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using _24_Picker.ViewModel;
+
+
+namespace _24_Picker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeyViewModel(); 
+        }
+    }
+}
---
+++ Controls/Views/_24_Picker/ViewModel/MonkeyViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _24_Picker.ViewModel
+{
+    public class MonkeyViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private List<string> _monkeys = [];     // Ensure _monkeys is initialized.
+        public List<string> Monkeys {
+            get => _monkeys; 
+            set {
+                _monkeys = value;
+                OnPropertyChanged(nameof(Monkeys));
+            }
+        }
+
+        public MonkeyViewModel() {
+            Monkeys = [
+                "Baboon",
+                "Capuchin Monkey",
+                "Blue Monkey",
+                "Squirrel Monkey",
+                "Golden Lion Tamarin",
+                "Howler Monkey",
+                "Japanese Macaque"
+            ];
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private string? _selectedMonkey;
+        public string? SelectedMonkey {
+            get => _selectedMonkey;
+            set {
+                _selectedMonkey = value;
+                OnPropertyChanged(nameof(SelectedMonkey));
+            }
+        }
+    }
+}

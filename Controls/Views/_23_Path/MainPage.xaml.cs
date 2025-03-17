1ba1ce90aba2c81f91d41226a1aaacef2b076b02
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-03-17 20:48:43 +0530

    _23 Path Class is added

---
+++ Controls/Views/_23_Path/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _23_Path
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); 
+        }
+    }
+            
+}
---
+++ Controls/Views/_23_Path/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace _23_Path;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

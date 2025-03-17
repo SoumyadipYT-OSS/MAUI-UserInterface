30c3ffd76a2ce272af047f542f8f0de3c07fb09e
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-03-17 21:00:42 +0530

    _29 ScrollView Class is updated

---
+++ Controls/Views/_29_ScrollView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _29_ScrollView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Controls/Views/_29_ScrollView/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using System.Diagnostics;
+
+namespace _29_ScrollView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // The Scrolled event provides real-time coordinates for scroll position changes.
+        private void OnScrolled(object sender, ScrolledEventArgs e) {
+            Debug.WriteLine($"Scrolled to (X: {e.ScrollX}, Y: {e.ScrollY})");
+        }
+    }
+
+}
